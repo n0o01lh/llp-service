6b7a87532a068621fa77938643b43631361d7297
Nicolas Onate <nionate.on@gmail.com>
2024-08-21 14:51:04 -0400

    adds healthcheck default configuration

--- src/main.go
+++ src/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+)
 
 func main() {
 	app := fiber.New()
@@ -8,6 +11,8 @@ func main() {
 	app.Get("/", func(context *fiber.Ctx) error {
 		return context.SendString("Hello, World! changed v4")
 	})
+
+	app.Use(healthcheck.New())
 
 	app.Listen(":3000")
 }
