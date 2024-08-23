2be442cc8a0ab79c9d8025dfbd88a66988b6546d
n0o01lh <92607073+n0o01lh@users.noreply.github.com>
2024-08-23 11:08:47 -0400

    adds course crud (#4)

---
+++ internals/core/services/course_service.go
@@ -0,0 +1,67 @@
+package services
+
+import (
+	"github.com/n0o01lh/llp/internals/core/domain"
+	"github.com/n0o01lh/llp/internals/core/ports"
+)
+
+type CourseService struct {
+	courseRepository ports.CourseRepository
+}
+
+func NewCourseService(courseRepo ports.CourseRepository) *CourseService {
+	return &CourseService{
+		courseRepository: courseRepo,
+	}
+}
+
+var _ ports.CourseService = (*CourseService)(nil)
+
+func (service *CourseService) Create(course *domain.Course) (*domain.Course, error) {
+	courseCreated, err := service.courseRepository.Create(course)
+
+	if err != nil {
+		return nil, err
+	}
+	return courseCreated, nil
+}
+
+func (service *CourseService) ListAll() ([]*domain.Course, error) {
+	courseList, err := service.courseRepository.ListAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return courseList, nil
+}
+
+func (service *CourseService) FindOne(id uint) (*domain.Course, error) {
+	course, err := service.courseRepository.FindOne(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return course, nil
+}
+
+func (service *CourseService) Update(id uint, course *domain.Course) (*domain.Course, error) {
+	courseUpdated, err := service.courseRepository.Update(id, course)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return courseUpdated, nil
+}
+
+func (service *CourseService) Delete(id uint) error {
+	err := service.courseRepository.Delete(id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internals/repositories/course_repository.go
@@ -0,0 +1,89 @@
+package repositories
+
+import (
+	"errors"
+
+	"github.com/n0o01lh/llp/internals/core/domain"
+	"github.com/n0o01lh/llp/internals/core/ports"
+	"gorm.io/gorm"
+)
+
+type CourseRepository struct {
+	Database *gorm.DB
+}
+
+func NewCourseRepository(db *gorm.DB) *CourseRepository {
+	return &CourseRepository{
+		Database: db,
+	}
+}
+
+var _ ports.CourseRepository = (*CourseRepository)(nil)
+
+func (r *CourseRepository) Create(course *domain.Course) (*domain.Course, error) {
+
+	var newCourse *domain.Course
+
+	r.Database.Create(&course)
+	r.Database.Find(&newCourse, course.Id)
+
+	if newCourse == nil {
+		return nil, errors.New("course not created")
+	}
+
+	return newCourse, nil
+}
+
+func (r *CourseRepository) ListAll() ([]*domain.Course, error) {
+
+	var courseList []*domain.Course
+
+	r.Database.Find(&courseList)
+
+	if courseList == nil {
+		return nil, errors.New("courses not found")
+	}
+
+	return courseList, nil
+}
+
+func (r *CourseRepository) FindOne(id uint) (*domain.Course, error) {
+
+	var course *domain.Course
+
+	row := r.Database.Find(&course, id)
+
+	if row.RowsAffected == 0 {
+		return nil, errors.New("course not found")
+	}
+
+	return course, nil
+}
+
+func (r *CourseRepository) Update(id uint, course *domain.Course) (*domain.Course, error) {
+
+	var updatedCourse *domain.Course
+
+	response := r.Database.Where("id = ?", id).Updates(&course)
+
+	r.Database.Find(&updatedCourse, id)
+
+	if response.RowsAffected == 0 {
+		return nil, errors.New("course cannot be updated")
+	}
+
+	return updatedCourse, nil
+}
+
+func (r *CourseRepository) Delete(id uint) error {
+
+	var course *domain.Course
+
+	result := r.Database.Delete(&course, id)
+
+	if result.RowsAffected == 0 {
+		return errors.New("course not found")
+	}
+
+	return nil
+}
