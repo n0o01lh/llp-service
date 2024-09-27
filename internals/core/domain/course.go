2f4371409159f23fe474f68f4c5a59e6f850f866
n0o01lh <92607073+n0o01lh@users.noreply.github.com>
2024-09-26 21:41:37 -0300

    adds teacher_id to course entity and adds validation for create course (#20)

--- internals/core/domain/course.go
+++ internals/core/domain/course.go
@@ -2,17 +2,19 @@ package domain
 
 type Course struct {
 	Id          uint        `json:"id"`
-	Title       string      `json:"title"`
-	Description string      `json:"description"`
+	Title       string      `json:"title" validate:"required"`
+	Description string      `json:"description" validate:"required"`
 	Price       float64     `json:"price"`
+	Teacher_id  int         `json:"teacher_id" validate:"required"`
 	Resources   []*Resource `gorm:"many2many:resources_courses;"`
 }
 
-func NewCourse(id uint, title, description string, price float64) *Course {
+func NewCourse(id uint, teacher_id int, title, description string, price float64) *Course {
 	return &Course{
 		Id:          id,
 		Title:       title,
 		Description: description,
+		Teacher_id:  teacher_id,
 		Price:       price,
 	}
 }
