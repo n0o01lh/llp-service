5bb74233e21807a534d191752bf3adc1c694b66c
Nicolas Onate <nionate.on@gmail.com>
2024-10-17 16:35:30 -0300

    deleting resource course relationships before delete a course

--- internals/core/domain/course.go
+++ internals/core/domain/course.go
@@ -1,12 +1,12 @@
 package domain
 
 type Course struct {
-	Id          uint        `json:"id"`
+	Id          uint        `json:"id" gorm:"primaryKey;size:256"`
 	Title       string      `json:"title" validate:"required"`
 	Description string      `json:"description" validate:"required"`
 	Price       float64     `json:"price"`
 	Teacher_id  int         `json:"teacher_id" validate:"required"`
-	Resources   []*Resource `json:"resources" gorm:"many2many:resources_courses;"`
+	Resources   []*Resource `json:"resources" gorm:"many2many:resources_courses;constraint:OnDelete:CASCADE"`
 }
 
 func NewCourse(id uint, teacher_id int, title, description string, price float64) *Course {
