99a68e15b7ccfe2f22573f056a38233792167fb0
Nicolas Onate <nionate.on@gmail.com>
2024-11-01 15:56:08 -0300

    adds order by to sales query

--- internals/repositories/queries/reource_sales_history_by_teacher_query.go
+++ internals/repositories/queries/reource_sales_history_by_teacher_query.go
@@ -4,4 +4,4 @@ const RESOURCE_SALES_HISTORY_BY_TEACHER_QUERY = `select distinct sh.resource_id,
 from public.sales_history sh 
 left join public.resources as r on r.id = sh.resource_id 
 where sh.teacher_id = ? 
-group by sh.resource_id, sh.teacher_id, r.title`
+group by sh.resource_id, sh.teacher_id, r.title order by amount desc;`
